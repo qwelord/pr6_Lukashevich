6366fad65eb75902aa0b6d300307736de205b685
Linashihova <linashikhova@mail.ru.>
2026-05-18 11:22:53 +0500

    азработанный контекст для таблицы записок. укашевич

---
+++ app/src/main/java/com/example/notes_lukashevich/datas/NotesContext.java
@@ -0,0 +1,50 @@
+package com.example.notes_lukashevich.datas;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import com.example.notes_lukashevich.domains.models.Note;
+import java.util.ArrayList;
+
+public class NotesContext {
+
+    public static ArrayList<Note> AllNotes() {
+        ArrayList<Note> allNotes = new ArrayList<>();
+        Cursor cursor = DbContext.sqLiteDatabase.query("Notes", null, null, null, null, null, null);
+
+        if (cursor.moveToFirst() == false) {
+            cursor.close();
+            return allNotes;
+        }
+
+        do {
+            Note note = new Note();
+            note.id = cursor.getInt(0);
+            note.title = cursor.getString(1);
+            note.text = cursor.getString(2);
+            note.date = cursor.getString(3);
+            note.color = cursor.getString(4);
+            allNotes.add(note);
+        } while (cursor.moveToNext());
+
+        cursor.close();
+        return allNotes;
+    }
+
+    public static void Save(Note note, boolean update) {
+        ContentValues CV = new ContentValues();
+        CV.put("Title", note.title);
+        CV.put("Text", note.text);
+        CV.put("Date", note.date);
+        CV.put("Color", note.color);
+
+        if (update == false) {
+            DbContext.sqLiteDatabase.insert("Notes", null, CV);
+        } else {
+            DbContext.sqLiteDatabase.update("Notes", CV, "Id = ?", new String[] {String.valueOf(note.id)});
+        }
+    }
+
+    public static void Delete(Note note) {
+        DbContext.sqLiteDatabase.delete("Notes", "Id = ?", new String[] {String.valueOf(note.id)});
+    }
+}
